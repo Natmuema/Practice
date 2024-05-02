94ed387455507007fab5dcdabc7ed7a76a7fc6e4
Kalondu Muema <nkalondumuema@gmail.com>
2024-05-02 11:00:32 +0300

    first assignment

---
+++ assignment.js
@@ -0,0 +1,17 @@
+// Function to Find the Area of Rectangle
+function areaRectangle(a, b) {
+    let area = a * b;
+    return area;
+}
+
+// Function to Find the Perimeter of Rectangle
+function perimeterRectangle(a, b) {
+    let perimeter = 2 * (a + b);
+    return perimeter;
+}
+
+
+let a = 5;
+let b = 6;
+console.log("Area = " + areaRectangle(a, b));
+console.log("Perimeter = " + perimeterRectangle(a, b)); 
